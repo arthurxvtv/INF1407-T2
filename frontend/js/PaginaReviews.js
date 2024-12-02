3ac5a7d8e33a5e89bff9119e5f0b310f49a9cfe9
arthurxvtv <90696992+arthurxvtv@users.noreply.github.com>
2024-12-02 11:20:02 -0300

    Criação de página para criação de reviews

--- frontend/js/PaginaReviews.js
+++ frontend/js/PaginaReviews.js
@@ -2,7 +2,6 @@
 window.onload = function () {
     var listaReviews = document.getElementById("lista-reviews");
     var logoutButton = document.getElementById("logout-button");
-    var createReviewButton = document.getElementById("create-review-button");
     var controller = new ReviewsController();
     controller.listaReviews().then(function (reviews) {
         reviews.map(function (review) {
@@ -13,14 +12,5 @@ window.onload = function () {
     });
     logoutButton === null || logoutButton === void 0 ? void 0 : logoutButton.addEventListener("click", function () {
         controller.logout();
-    });
-    createReviewButton === null || createReviewButton === void 0 ? void 0 : createReviewButton.addEventListener("click", function () {
-        var gameNameInput = document.getElementById("game-name");
-        var starsInput = document.getElementById("stars");
-        controller.createReview(gameNameInput.value, parseInt(starsInput.value)).then(function (review) {
-            var el = document.createElement("li");
-            el.innerHTML = "".concat(review.game_name, " - ").concat(review.stars, " estrelas.");
-            listaReviews === null || listaReviews === void 0 ? void 0 : listaReviews.appendChild(el);
-        });
     });
 };
--- frontend/ts/PaginaReviews.ts
+++ frontend/ts/PaginaReviews.ts
@@ -1,7 +1,6 @@
 window.onload = function () {
   const listaReviews = document.getElementById("lista-reviews");
   const logoutButton = document.getElementById("logout-button");
-  const createReviewButton = document.getElementById("create-review-button");
 
   const controller = new ReviewsController();
 
@@ -15,16 +14,5 @@ window.onload = function () {
 
   logoutButton?.addEventListener("click", function () {
     controller.logout();
-  });
-
-  createReviewButton?.addEventListener("click", function () {
-    const gameNameInput = document.getElementById("game-name") as HTMLInputElement;
-    const starsInput = document.getElementById("stars") as HTMLInputElement;
-
-    controller.createReview(gameNameInput.value, parseInt(starsInput.value)).then(function (review) {
-      const el = document.createElement("li");
-      el.innerHTML = `${review.game_name} - ${review.stars} estrelas.`;
-      listaReviews?.appendChild(el);
-    });
   });
 };
